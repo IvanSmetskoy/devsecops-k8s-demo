6f951790c2e4cb4ac7b7e4b2edb043d96c1aa761
Ivan Smetskoy <ivan.smetskoi@gmail.com>
2023-04-26 20:41:13 +0600

    update pom file

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -4,8 +4,8 @@
 
 echo $imageName
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.40.0 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.40.0 -q image --exit-code 1 --severity CRITICAL --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.40.0 -q image --exit-code 1 --severity LOW,MEDIUM,HIGH --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.40.0 -q image --exit-code 0 --severity CRITICAL --light $imageName
 
 # Trivy Scan results processing
 exit_code=$?
@@ -16,5 +16,5 @@ if [[ "${exit_code}" == 1 ]]; then
     echo "Image scanning failed. Vulnerabilities found"
     exit 1;
 else
-    echo "Image scanning passed. No CRITICAL vulnerabilities found"
+    echo "Image scanning passed. No vulnerabilities found"
 fi;
