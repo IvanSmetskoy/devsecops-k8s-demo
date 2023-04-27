6f59e2316591dee61a050c7fa66fef7f8ee0453a
Ivan Smetskoy <ivan.smetskoi@gmail.com>
2023-04-27 14:56:48 +0600

    added OWASP ZAP DAST Scan stage

--- Jenkinsfile
+++ Jenkinsfile
@@ -127,6 +127,14 @@ pipeline {
         }
       }
 
+      stage('OWASP ZAP - DAST') {
+        steps {
+          withKubeConfig([credentialsId: 'kubeconfig']) {
+            sh 'bash zap.sh'
+          }
+        }
+      }
+
     }
 
     post {
---
+++ zap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# zap.sh
+
+PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+
+# first run this
+chmod 777 $(pwd)
+echo $(id -u):$(id -g)
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+
+# to tun with custom rules:
+# docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap-rules -r zap_report.html
+
+# HTML Report
+sudo mkdir -p owasp-zap-report
+sudo mv zap_report.html owasp-zap-report
+
+exit_code=$?
+echo "Exit Code : $exit_code"
+
+  if [[ ${exit_code} -ne 0 ]]; then
+     echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
+     exit 1;
+    else
+     echo "OWASP ZAP did not report any Risk"
+  fi;
+
+# Generate ConfigFile
+# docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t http://devsecops-demo.eastus.cloudapp.azure.com:31933/v3/api-docs -f openapi -g gen_file
