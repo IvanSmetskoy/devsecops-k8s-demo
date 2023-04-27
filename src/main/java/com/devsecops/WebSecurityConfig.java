22d9bbab12c0d8f85eecfe1ce2130dd540018629
Ivan Smetskoy <ivan.smetskoi@gmail.com>
2023-04-27 17:12:25 +0600

    added Spring Boot Security

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -9,11 +9,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/register").permitAll().antMatchers("/welcome")
-				.hasAnyRole("USER", "ADMIN").antMatchers("/getEmployees").hasAnyRole("USER", "ADMIN")
-				.antMatchers("/addNewEmployee").hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+        //http.csrf().disable();
 
-		//http.csrf().disable();
-	}
+    }
 }
